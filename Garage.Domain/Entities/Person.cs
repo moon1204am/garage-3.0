55a72d6f584c42ae2c76fc4a096f4f0337457567
Sammy Wong <sammywong528@outlook.com>
2023-11-20 13:47:37 +0100

    Validering av personnummer

--- Garage.Domain/Entities/Person.cs
+++ Garage.Domain/Entities/Person.cs
@@ -14,6 +14,8 @@ namespace Garage.Domain.Entities
     
         public int PersonId { get; set; }
         [Required(ErrorMessage = "Du måste ange ett personnumer")]
+        [StringLength(12, MinimumLength = 12, ErrorMessage = "Personnummer måste vara 12-siffrigt.")]
+        [RegularExpression(@"^(19|20)?[0-9]{6}$", ErrorMessage = "Ogiltigt personnummer.")]
         [Display(Name = "Personnummer")]
         public string SSN { get; set; } = string.Empty;
         [Required(ErrorMessage = "Du måste ange ett förnamn")]
