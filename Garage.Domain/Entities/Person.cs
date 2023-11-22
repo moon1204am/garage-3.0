8bd7859721e7fff1169a02ae2c26c1ef61e48866
Sammy Wong <sammywong528@outlook.com>
2023-11-22 14:27:35 +0100

    Add personViewModel

--- Garage.Domain/Entities/Person.cs
+++ Garage.Domain/Entities/Person.cs
@@ -12,18 +12,12 @@ namespace Garage.Domain.Entities
     public class Person
     {
      
-    
         public int PersonId { get; set; }
-        [Display(Name = "Social Security Number"), Required(ErrorMessage = "SSN is required.")]
-        [RegularExpression(@"^(19|20)?[0-9]{6}[-+]?[0-9]{6}$", ErrorMessage = "Invalid SSN, it should be 12 digits.")]
-        public string SSN { get; set; } = string.Empty;
         
-        [Display(Name = "Last Name"), Required(ErrorMessage = "A last name is required")]
+        public string SSN { get; set; } = string.Empty;
         
         public string LastName { get; set; } = string.Empty;
 
-        [Display(Name = "First Name"), Required(ErrorMessage = "A first name is required.")]
-        
         public string FirstName { get; set; } = string.Empty;
         //Nav prop
         public ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
---
+++ Garage.Web/Models/ViewModels/PersonViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Garage.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Garage.Web.Models.ViewModels
+{
+    public class PersonViewModel
+    {
+        public int PersonId { get; set; }
+
+        [Display(Name = "Social Security Number"), Required(ErrorMessage = "SSN is required.")]
+        [RegularExpression(@"^(19|20)?[0-9]{6}[-+]?[0-9]{6}$", ErrorMessage = "Invalid SSN, it should be 12 digits.")]
+        public string SSN { get; set; } = string.Empty;
+
+        [Display(Name = "Last Name"), Required(ErrorMessage = "A last name is required")]
+
+        public string LastName { get; set; } = string.Empty;
+
+        [Display(Name = "First Name"), Required(ErrorMessage = "A first name is required.")]
+
+        public string FirstName { get; set; } = string.Empty;
+        public ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
+
+    }
+}
