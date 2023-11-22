3a5c91f61b1e95aafe9b5ee83c5af0b3e1b02141
Sammy Wong <sammywong528@outlook.com>
2023-11-22 15:33:39 +0100

    Add view  and viewmodel for receipt

--- Garage.Web/Models/ViewModels/PersonViewModel.cs
+++ Garage.Web/Models/ViewModels/PersonViewModel.cs
@@ -8,7 +8,7 @@ namespace Garage.Web.Models.ViewModels
         public int PersonId { get; set; }
 
         [Display(Name = "Social Security Number"), Required(ErrorMessage = "SSN is required.")]
-        [RegularExpression(@"^(19|20)?[0-9]{6}[-+]?[0-9]{6}$", ErrorMessage = "Invalid SSN, it should be 12 digits.")]
+        [RegularExpression(@"^(19|20)?[0-9]{6}[-+]?[0-9]{4}$", ErrorMessage = "Invalid SSN, it should be 12 digits.")]
         public string SSN { get; set; } = string.Empty;
 
         [Display(Name = "Last Name"), Required(ErrorMessage = "A last name is required")]
---
+++ Garage.Web/Models/ViewModels/ReceiptViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage.Web.Models.ViewModels
+{
+    public class ReceiptViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "License number")]
+        public string LicenseNr { get; set; }
+
+        public DateTime Arrival { get; set; }
+
+        [Display(Name = "Check out")]
+        public DateTime CheckOut { get; set; }
+
+        [Display(Name = "Parking time")]
+        public string ParkingTime { get; set; }
+
+        [Display(Name = "Price/hour")]
+        public int Price { get; set; }
+
+        [Display(Name = "Total cost")]
+        public int TotalCost { get; set; }
+    }
+}
