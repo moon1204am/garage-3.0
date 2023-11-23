2fe3ba62f81580c0ceab454581b1755179a0d851
Sammy Wong <sammywong528@outlook.com>
2023-11-23 18:44:11 +0100

    Iminstalled NuGet ActiveLogin

--- Garage.Web/Models/ViewModels/PersonViewModel.cs
+++ Garage.Web/Models/ViewModels/PersonViewModel.cs
@@ -1,5 +1,4 @@
-﻿using ActiveLogin.Identity.Swedish.AspNetCore.Validation;
-using Garage.Domain.Entities;
+﻿using Garage.Domain.Entities;
 using System.ComponentModel.DataAnnotations;
 
 namespace Garage.Web.Models.ViewModels
@@ -13,7 +12,6 @@ namespace Garage.Web.Models.ViewModels
         //(01|02|03|04|05|06|07|08|09|10|11|12)((0[1-9])|(1|2)[0-9]|(30|31))-\d{4}$", ErrorMessage = "Invalid SSN, it should be 12 digits.")]
         //[RegularExpression(@"^[0-9]{8}[-+]?[0-9]{4}$", ErrorMessage = "Invalid SSN, please enter a new one")]
         //[RegularExpression(@"^(\d{6}|\d{8})-\d{4}$", ErrorMessage = "Invalid SSN, please enter a new one")]
-        //[PersonalIdentityNumber]
         public string SSN { get; set; } = string.Empty;
 
         [Display(Name = "Last name"), Required(ErrorMessage = "A last name is required")]
